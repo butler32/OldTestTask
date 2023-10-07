3b793f5540419b4fcd1f1cb87bc5f0670a2fe61a
Владимир Дворецкий <plus88228@gmail.com>
2023-10-07 15:49:38 +0300

    Add project files.

---
+++ TestTask/Services/Implemintations/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implemintations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            var orderWithMaxPrice = _context.Orders
+                                    .OrderByDescending(u => u.Price)
+                                    .FirstOrDefault();
+            return Task.FromResult(orderWithMaxPrice);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return Task.FromResult(_context.Orders.Where(q => q.Quantity > 10).ToList());
+        }
+    }
+}
---
+++ TestTask/Services/Implemintations/UserService.cs
@@ -0,0 +1,29 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implemintations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<User> GetUser()
+        {
+            var userWithMaxOrders = _context.Users
+                                    .OrderByDescending(u => u.Orders.Count)
+                                    .FirstOrDefault();
+
+            return Task.FromResult(userWithMaxOrders);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return Task.FromResult(_context.Users.Where(i => i.Status == Enums.UserStatus.Inactive).ToList());
+        }
+    }
+}
